775cf9dd3bb09a77c69850478aaa8989fbe0bb83
Iwan Purwadi <36619633+nawiq7208@users.noreply.github.com>
2022-05-29 20:58:34 +0800

    add dependency

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD9cpnbybbUpc_PgCL-M1TX2uqrWGUiDGY",
+  authDomain: "chat-app-react-1bedf.firebaseapp.com",
+  projectId: "chat-app-react-1bedf",
+  storageBucket: "chat-app-react-1bedf.appspot.com",
+  messagingSenderId: "1012214848265",
+  appId: "1:1012214848265:web:322a104cb3c2e1b45551d8"
+};
+
+export default firebaseConfig;
