eebfd616652e0c94e286894b133977d17237c3a9
Iwan Purwadi <36619633+nawiq7208@users.noreply.github.com>
2022-06-10 23:06:42 +0800

    Initial Commit

--- src/pages/private/chat/index.js
+++ src/pages/private/chat/index.js
@@ -1,5 +1,19 @@
 import React from "react";
+import Button from "@material-ui/core/Button/Button";
+import { auth } from "../../../components/FirebaseProvider";
 
 export default function Chat() {
-  return <h1>Halaman Chat</h1>;
+  return (
+    <>
+      <h1>Halaman Chat</h1>
+      <Button
+        variant="contained"
+        onClick={() => {
+          auth.signOut();
+        }}
+      >
+        Logout
+      </Button>
+    </>
+  );
 }
